b848407e10673dde28d967518aa1fe80ae703d86
sarahlte <sarah_leconte@msn.com>
2025-01-13 16:01:34 +0100

    Homepage
    Mise en place des éléments de la page d'accueil

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,46 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faUsers, faRobot, faPlay } from '@fortawesome/free-solid-svg-icons'
+
 export default function Homepage() {
     return (
         <div>
-            <h1>Homepage</h1>
+            <h1>Choose your game mode</h1>
+            <div className="flex">
+                <div className="card card-side bg-base-100 shadow-xl mx-4 w-96">
+                    <figure>
+                        <FontAwesomeIcon icon={faRobot} size="2xl" />
+                    </figure>
+                    <div className="card-body py-0">
+                        <h2 className="card-title">Bot mode</h2>
+                        <div className="card-actions justify-start">
+                            <p className="grow-0">Game against bot</p>
+                        </div>
+                        <div className="card-actions justify-end">
+                            <button className="yellow-btn">
+                                <FontAwesomeIcon icon={faPlay} className="mr-2" />
+                                Play
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card card-side bg-base-100 shadow-xl mx-4 w-69">
+                    <figure>
+                        <FontAwesomeIcon icon={faUsers} size="2xl"/>
+                    </figure>
+                    <div className="card-body py-0">
+                        <h2 className="card-title">Player mode</h2>
+                        <div className="card-actions justify-start">
+                            <p className="grow-0">Game against local player</p>
+                        </div>                        
+                        <div className="card-actions justify-end">
+                            <button className="yellow-btn">
+                                <FontAwesomeIcon icon={faPlay} className="mr-2" />
+                                Play
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
