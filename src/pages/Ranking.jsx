146222b736e4eb6aeab2515c5e99e6bb23d08df0
sarahlte <sarah_leconte@msn.com>
2025-01-16 14:22:07 +0100

    Update Ranking.jsx

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -4,95 +4,92 @@ import second from '../assets/2nd-place.png'
 import third from '../assets/3rd-place.png'
 
 export default function Ranking() {
-    const [players, setPlayers] = useState([{rank: 1, name:'test'}, {rank:2, name:'test2'}, {rank: 3, name:'test3'}, {rank: 4, name:'test4'}, {rank:5, name:'Test5'}])
+    const [players, setPlayers] = useState([{rank: 1, name:'test', bestScore:12}, {rank:2, name:'test2', bestScore: 11}, {rank: 3, name:'test3', bestScore: 8}, {rank: 4, name:'test4', bestScore: 5}, {rank:5, name:'Test5', bestScore: 3}])
 
-    function PlayersRank({rank, name}) {
+    function PlayersRank({rank, name, bestScore}) {
         if(rank == 1){
             return (
-                <li className="pt-3 pb-0 sm:pt-4">
-                    <div className="flex items-center space-x-4 rtl:space-x-reverse">
-                        <div className="flex-shrink-0">
+                <tr className="pt-3 pb-0 sm:pt-4">
+                        <td className="flex justify-center">
                             <img className="w-8 h-8" src={first} alt="easy icon"/>
-                        </div>
-                        <div className="flex-1 min-w-0">
+                        </td>
+                        <td className="min-w-0">
                             <p className="text-sm font-bold truncate">
                                 {name}
                             </p>
-                        </div>
-                        <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                            <p></p>                    
-                        </div>
-                    </div>
-                </li>
+                        </td>
+                        <td className="flex justify-center">
+                            <p>{bestScore}</p>                    
+                        </td>
+                </tr>
             )
         } else if (rank == 2){
             return (
-                <li className="pt-3 pb-0 sm:pt-4">
-                    <div className="flex items-center space-x-4 rtl:space-x-reverse">
-                        <div className="flex-shrink-0">
+                <tr className="pt-3 pb-0 sm:pt-4">
+                        <td className="flex justify-center">
                             <img className="w-8 h-8" src={second} alt="easy icon"/>
-                        </div>
-                        <div className="flex-1 min-w-0">
+                        </td>
+                        <td className="min-w-0">
                             <p className="text-sm font-bold truncate">
                                 {name}
                             </p>
-                        </div>
-                        <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                            <p></p>                    
-                        </div>
-                    </div>
-                </li>
+                        </td>
+                        <td className="flex justify-center">
+                            <p>{bestScore}</p>                    
+                        </td>
+                </tr>
             ) 
         } else if (rank == 3){
             return (
-                <li className="pt-3 pb-0 sm:pt-4">
-                    <div className="flex items-center space-x-4 rtl:space-x-reverse">
-                        <div className="flex-shrink-0">
+                <tr className="pt-3 pb-0 sm:pt-4">
+                        <td className="flex justify-center">
                             <img className="w-8 h-8" src={third} alt="easy icon"/>
-                        </div>
-                        <div className="flex-1 min-w-0">
+                        </td>
+                        <td className="min-w-0">
                             <p className="text-sm font-bold truncate">
                                 {name}
                             </p>
-                        </div>
-                        <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                            <p></p>                    
-                        </div>
-                    </div>
-                </li>
+                        </td>
+                        <td className="flex justify-center">
+                            <p>{bestScore}</p>                    
+                        </td>
+                </tr>
             )
         }
         return (
-            <li className="pt-3 pb-0 sm:pt-4">
-                <div className="flex items-center space-x-4 rtl:space-x-reverse">
-                    <div className="flex-shrink-0">
-                        <p className='w-8 h-8'>{rank}</p>
-                    </div>
-                    <div className="flex-1 min-w-0">
+            <tr className="pt-3 pb-0 sm:pt-4">
+                    <td className="flex justify-center">
+                        <p className='w-8 h-8 text-center'>{rank}</p>
+                    </td>
+                    <td className="min-w-0">
                         <p className="text-sm font-bold truncate">
                             {name}
                         </p>
-                    </div>
-                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                        <p></p>                    
-                    </div>
-                </div>
-            </li>
+                    </td>
+                    <td className="flex justify-center">
+                        <p>{bestScore}</p>                    
+                    </td>
+            </tr>
         )
     }
 
     return (
-        <div className="flex flex-col items-center">        
-            <ul className="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
-                <li className="pb-3 sm:pb-4">
-                    <div className="flex items-center justify-start space-x-4 rtl:space-x-reverse">
-                        <h1>Ranking</h1>
-                    </div>
-                </li>
-                {players.map((player) => (
-                    <PlayersRank key={player.rank} rank = {player.rank} name={player.name} />
-                ))}
-            </ul>
+        <div className="flex flex-col items-center">  
+            <h1>Ranking</h1>  
+            <table className='table w-[50%]'>
+                <thead>
+                    <tr>
+                        <th className='flex justify-center'>Rank</th>
+                        <th>User name</th>
+                        <th className='flex justify-center'>Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {players.map((player) => (
+                        <PlayersRank key={player.rank} rank = {player.rank} name={player.name} bestScore={player.bestScore} />
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
