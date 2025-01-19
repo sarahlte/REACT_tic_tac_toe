8b33aad2eabec1c29d89bfe5f8e24536fd67fcd2
sarahlte <sarah_leconte@msn.com>
2025-01-19 18:29:57 +0100

    Add css property to 3hit first click
    add css property to identify the position will disapeared

--- src/components/Game/Board.jsx
+++ src/components/Game/Board.jsx
@@ -25,19 +25,19 @@ export default function Board({player, player1, player2, p1Victory, p2Victory, t
           </div>
           <div className='board'>
           <div className="board-row">
-            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />        
-            <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+            <Square id={0} value={squares[0]} onSquareClick={() => handleClick(0)} />
+            <Square id={1} value={squares[1]} onSquareClick={() => handleClick(1)} />        
+            <Square id={2} value={squares[2]} onSquareClick={() => handleClick(2)}/>
           </div>
           <div className="board-row">
-            <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-            <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-            <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+            <Square id={3} value={squares[3]} onSquareClick={() => handleClick(3)}/>
+            <Square id={4} value={squares[4]} onSquareClick={() => handleClick(4)}/>
+            <Square id={5} value={squares[5]} onSquareClick={() => handleClick(5)}/>
           </div>
           <div className="board-row">
-            <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-            <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-            <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+            <Square id={6} value={squares[6]} onSquareClick={() => handleClick(6)}/>
+            <Square id={7} value={squares[7]} onSquareClick={() => handleClick(7)}/>
+            <Square id={8} value={squares[8]} onSquareClick={() => handleClick(8)}/>
           </div>
         </div>
         <div className='game-info flex justify-center items-center mt-5'>
--- src/components/Game/Square.jsx
+++ src/components/Game/Square.jsx
@@ -1,37 +1,37 @@
 import cross from "../../assets/cross.svg";
 import circle from "../../assets/circle.svg";
 
-export default function Square({ value, onSquareClick }) {
+export default function Square({ value, onSquareClick, id }) {
   if (value == 'X'){
       return (
-        <div className="square darkgrey-btn w-28 h-28" onClick={onSquareClick}>
+        <div id={'a'+id} className="square darkgrey-btn w-28 h-28" onClick={onSquareClick}>
           <img src={cross} alt="X"/>
         </div>
       );
   } else if (value == 'O') {
     return (
-      <div className="square darkgrey-btn w-28 h-28" onClick={onSquareClick}>
+      <div id={'a'+id} className="square darkgrey-btn w-28 h-28" onClick={onSquareClick}>
         <img src={circle} alt="O" />
       </div>
     );
   } 
   else if (value == 'VX'){
     return (
-      <div className="square darkgrey-btn w-28 h-28  primary-filter" onClick={onSquareClick}>
+      <div id={'a'+id} className="square darkgrey-btn w-28 h-28  primary-filter" onClick={onSquareClick}>
         <img src={cross} alt="X" />
       </div>
     );
   }
   else if (value == 'VO'){
     return (
-      <div className="square darkgrey-btn w-28 h-28 secondary-filter" onClick={onSquareClick}>
+      <div id={'a'+id} className="square darkgrey-btn w-28 h-28 secondary-filter" onClick={onSquareClick}>
         <img src={circle} alt="O" />
       </div>
     );
   }
   else {
     return (
-      <div className="square darkgrey-btn w-28 h-28" onClick={onSquareClick}>
+      <div id={'a'+id} className="square darkgrey-btn w-28 h-28" onClick={onSquareClick}>
         {value}
       </div>
     );
